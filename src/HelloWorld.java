989266b1c40c6ddf2c8542af1ff87115e9ffb757
PoloBaca <thebigbaca@gmail.com>
2023-12-17 18:29:43 -0600

    commit #2 curso java

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,31 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World from Java");
+        
+        String saludar = "Hello World from Java";
+        System.out.println(saludar);
+        System.out.println("saludar = " + saludar.toUpperCase());
+        
+        int numero = 11;
+
+        boolean valor = true;
+        int numero2 = 5;
+        if (valor){
+            System.out.println("numero = " + numero);
+            numero2 = 10;
+        }
+
+        System.out.println("numero2 = " + numero2);
+
+        var numero3 = "15";
+
+        String nombre;
+
+        nombre = "Andres";
+
+        if(numero > 10){
+            nombre = "Juan";
+
+        }
+        System.out.println("nombre = " + nombre);
     }
 }
