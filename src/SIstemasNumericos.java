438c3017258663d70014cc50a280eca96c2423f2
PoloBaca <thebigbaca@gmail.com>
2023-12-19 23:54:58 -0600

    commit 19 dec

--- src/SIstemasNumericos.java
+++ src/SIstemasNumericos.java
@@ -1,20 +1,41 @@
+import javax.swing.*;
+
 public class SIstemasNumericos {
     public static void main(String[] args) {
 
-        int numeroDecimal = 500;
+        String numeroStr = JOptionPane.showInputDialog(null, "Ingrese un numero entero:");
+        int numeroDecimal = 0;
+        try{
+            numeroDecimal = Integer.parseInt(numeroStr); Integer.parseInt(numeroStr);
+        } catch (NumberFormatException e) {
+            JOptionPane.showMessageDialog(null, "Error - Ingrese un numero entero");
+            main(args);
+            System.exit(0);
+        }
+
         System.out.println("numeroDecimal = " + numeroDecimal);
 
-        System.out.println("numero Binario de = " + numeroDecimal + " = " + Integer.toBinaryString(numeroDecimal));
+        String mensajeBinario = "numero Binario de = " + numeroDecimal + " = " + Integer.toBinaryString(numeroDecimal);
+        System.out.println(mensajeBinario);
         
         int numeroBinario = 0b111110100;
         System.out.println("numeroBinario = " + numeroBinario);
 
-        System.out.println("numero Octal de = " + numeroDecimal + " = " + Integer.toOctalString(numeroDecimal));
+        String mensajeOctal = "numero Octal de = " + numeroDecimal + " = " + Integer.toOctalString(numeroDecimal);
+        System.out.println(mensajeOctal);
         int numeroOctal = 0764;
         System.out.println("numero Octal = " + numeroOctal);
 
-        System.out.println("numero Hexadecimal = " + numeroDecimal + " = " + Integer.toHexString(numeroDecimal));
+        String mensajeHexa = "numero Hexadecimal = " + numeroDecimal + " = " + Integer.toHexString(numeroDecimal);
+        System.out.println(mensajeHexa);
+
         int numeroHex = 0x1f4;
         System.out.println("numeroHex = " + numeroHex);
+
+        String mensaje = mensajeBinario;
+        mensaje += "\n" + mensajeOctal;
+        mensaje += "\n" + mensajeHexa;
+
+        JOptionPane.showMessageDialog(null,mensaje);
     }
 }
