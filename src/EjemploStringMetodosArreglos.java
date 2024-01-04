04634e7dccd0a7de907f3a6aae68ddb1e18b9890
PoloBaca <thebigbaca@gmail.com>
2024-01-03 23:01:45 -0600

    commit Jan 3.2

--- src/EjemploStringMetodosArreglos.java
+++ src/EjemploStringMetodosArreglos.java
@@ -4,8 +4,29 @@ public class EjemploStringMetodosArreglos {
         String trabalenguas = "trabalenguas";
         System.out.println("trabalenguas.toCharArray() = " + trabalenguas.toCharArray());
 
+        char [] arreglo = trabalenguas.toCharArray();
+        int largo = arreglo.length;
+        System.out.println("largo = " + largo);
+        for (int i = 0; i < largo; i++){
+            System.out.println("arreglo = " + arreglo[i]);
+        }
 
-        
+        System.out.println();
+        System.out.println("trabalenguas = " + trabalenguas.split("a"));
 
+        String [] arreglo2 = trabalenguas.split("a");
+        int l = arreglo2.length;
+        for (int j=0; j < l; j++){
+            System.out.println(arreglo2[j]);
+        }
+
+        String archivo = "alguna.imagen.js";
+        String[] archivoArr = archivo.split("[.]"); // \\.
+        System.out.println("l = " + l);
+        l = archivoArr.length;
+        for (int j=0; j < l; j++){
+            System.out.println(archivoArr[j]);
+        }
+        System.out.println("extension = " + archivoArr[l-1]);
     }
 }
